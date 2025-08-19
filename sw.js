d0bed89f4da3b1c36de874bc5d44a9d41f408c0d
rightleftmike <rightleftmike@gmail.com>
2025-08-19 12:19:25 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "fitness-tracker-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) =>
+      res || fetch(e.request).then((resp) => {
+        // optionally cache new GETs
+        return resp;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
