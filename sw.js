6bbdd4e9b3c079983de61e139117e1fabf307e93
rightleftmike <rightleftmike@gmail.com>
2025-08-19 14:57:39 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,24 @@
-const CACHE = "fitness-tracker-v2";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+const CACHE = 'health-tracker-v1';
+const ASSETS = ['./','./index.html','./manifest.webmanifest'];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener('activate', e=>{
+  e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener('fetch', e=>{
   e.respondWith(
-    caches.match(e.request).then((res) =>
-      res || fetch(e.request).catch(() => caches.match("./index.html"))
+    caches.match(e.request).then(res =>
+      res || fetch(e.request).then(r=>{
+        if(e.request.method==='GET' && r.status===200){
+          const copy = r.clone();
+          caches.open(CACHE).then(c=> c.put(e.request, copy));
+        }
+        return r;
+      }).catch(()=>res)
     )
   );
 });
